5b30d5db32d7d285ececc3571c6a372c95e65703
Kyle Stengline <kyle.stengline@gmail.com>
2016-08-31 11:37:50 -0400

    comments on code

--- src/Earth.java
+++ src/Earth.java
@@ -4,16 +4,19 @@ public class Earth {
 //Code is executed sequentially within this method
   public static void main(String args[]){
   
-    //New is used when calling the constructor method when creating a new object. Assigning the Human variable as tom and assigning that variable a new Human object
-    //Our object is called tom
+//New is used when calling the constructor method when creating a new object. Assigning the Human variable as tom and assigning that variable a new Human object
+//Our object is called tom
     Human tom;
     
+//Assigning tom the object Human
+//New keyword is used to call the constructor method when creating a new object
     tom = new Human();
     tom.age = 5;
     tom.eyeColor = "brown";
     tom.heightInInches = 73;
     tom.name = "Tom Hammond";
     
+//invoking methods on tom
     tom.speak();
     tom.walk();
     tom.work();
--- src/Human.java
+++ src/Human.java
@@ -2,6 +2,7 @@
 //Contstructor provides a way to construct objects
 //Constructor method always has the same name as a class
 public class Human {
+//Classes contains instructions for how objects can be created as well as how objects carry out certain behavior
   
   String name;
   int age;
@@ -9,10 +10,13 @@ public class Human {
   String eyeColor;
 
   //Constructor method
+  //Special method for creating or "birthing" humans. i.e Contstructing humans.
+  //Doesn't need a return type. We call this within another java file to reference it for the construction of Human
   public Human(){
      
   }
 
+//Instructions for how humans will behave.
   public void speak(){
     System.out.println("Hello, my name is " + name);
     System.out.println("I am " + heightInInches + " inches tall");
