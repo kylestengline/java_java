2d429c68d2b7b52b40a4b93e91f3bbfc79cfff7b
Kyle Stengline <kyle.stengline@gmail.com>
2016-09-06 19:02:47 -0400

    removes old work and implements new methods with new files

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+  public static void main(String[] args) {
+    Vehicle myCar = new Vehicle();
+    myCar.accelerate();
+  }
+}
--- src/Flyable.java
+++
@@ -1,9 +0,0 @@
-//This is an interface. It should always be named after the functionality of the action.
-//interfaces don't have bodies.
-public interface Flyable {
- //this method is called an abstract method.
- //An abstraction of the idea that something should be able to fly.
-  public void fly();
-
-
-}
