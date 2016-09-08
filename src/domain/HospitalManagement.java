4e971f7d4ce37c39860f7eb4042033161dcb1bb3
Kyle Stengline <kyle.stengline@gmail.com>
2016-09-08 17:14:09 -0400

    adds new branch for new lesson

---
+++ src/domain/HospitalManagement.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class HospitalManagement {
+
+  //Nurses
+  private void checkVitalSigns(){
+    System.out.println("Checking vitals");
+  }
+
+  private void drawBlood(){
+    System.out.println("Drawing blood");
+  }
+
+  private void cleanPatientArea(){
+    System.out.println("Cleaning...");
+  }
+
+  //Doctors
+  private void prescribeMedicine(){
+    System.out.println("Prescirbe medicine");
+  }
+
+  private void diagnosePatients(){
+    System.out.println("Diagnose Patient");
+  }
+}
